d8a18ccd1b7fb41fa7791fa8e1fa243b8fbe6261
AntoineBaldacchino <a.baldacchino.j@gmail.com>
2023-07-24 19:04:48 +0200

    [initcrspectrum] correctly merge commit from cherry-pick

--- src/fluids/cosmicrays/initcrspectrum.F90
+++ src/fluids/cosmicrays/initcrspectrum.F90
@@ -172,8 +172,7 @@ contains
    subroutine init_cresp
 
       use constants,       only: cbuff_len, I_ZERO, I_ONE, zero, one, three, ten, half, logten, LO, HI
-      use cr_data,         only: cr_mass, cr_sigma_N, cr_names, icr_spc, icr_H1, cr_spectral
-      use cresp_variables, only: clight_cresp
+      use cr_data,         only: cr_mass, cr_sigma_N, cr_names, cr_Z, icr_spc, icr_H1, cr_spectral, cr_table
       use dataio_pub,      only: printinfo, warn, msg, die, nh
       use diagnostics,     only: my_allocate_with_index, my_allocate, ma1d
       use global,          only: disallow_CRnegatives
@@ -590,8 +589,10 @@ contains
         write (msg, *) "[initcrspectrum:init_cresp] CR ", cr_names(icr_spc(j)), ": 4/3 * sigma_N / ( m * c ) = ", fsynchr(j)
         if (master) call printinfo(msg)
 
-        K_cresp_paral(j, 1:ncrb) = K_cr_paral(icr_spc(j)) * (p_mid_fix(1:ncrb) / p_diff(j))**K_cre_pow(j)
-        K_cresp_perp(j,  1:ncrb) = K_cr_perp(icr_spc(j))  * (p_mid_fix(1:ncrb) / p_diff(j))**K_cre_pow(j)
+        !print *, 'j : ', j , 'mass : ', cr_mass(j), 'Z : ', cr_Z(j)
+
+        K_cresp_paral(j, 1:ncrb) = K_cr_paral(icr_spc(j)) * (cr_mass(j)*p_mid_fix(1:ncrb) / (abs(cr_Z(j))*p_diff(j)))**K_cre_pow(j) ! Scale diffusion of all species to protons.
+        K_cresp_perp(j,  1:ncrb) = K_cr_perp(icr_spc(j))  * (cr_mass(j)*p_mid_fix(1:ncrb) / (abs(cr_Z(j))*p_diff(j)))**K_cre_pow(j)
 
         K_cresp_paral(j, ncrb+1:ncr2b) = K_cresp_paral(j, 1:ncrb)
         K_cresp_perp (j, ncrb+1:ncr2b) = K_cresp_perp (j, 1:ncrb)
